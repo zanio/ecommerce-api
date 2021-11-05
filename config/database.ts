220845b18c211552e674a9e24eb1554b93c7f434
Aniefiok Akpan <akp.ani@yahoo.com>
2021-11-05 13:28:56 +0100

    chore(deploy): complete deplloyment

--- config/database.ts
+++ config/database.ts
@@ -1,67 +1,41 @@
-/**
- * Config source: https://git.io/JesV9
- *
- * Feel free to let us know via PR, if you find something broken in this config
- * file.
- */
-
 import Env from '@ioc:Adonis/Core/Env'
 import { OrmConfig } from '@ioc:Adonis/Lucid/Orm'
 import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
-const Url = require('url-parse')
-const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
+import  Application  from '@ioc:Adonis/Core/Application';
 
 const databaseConfig: DatabaseConfig & { orm: Partial<OrmConfig> } = {
-  /*
-  |--------------------------------------------------------------------------
-  | Connection
-  |--------------------------------------------------------------------------
-  |
-  | The primary connection for making database queries across the application
-  | You can use any key from the `connections` object defined in this same
-  | file.
-  |
-  */
   connection: Env.get('DB_CONNECTION'),
 
   connections: {
-    /*
-    |--------------------------------------------------------------------------
-    | PostgreSQL config
-    |--------------------------------------------------------------------------
-    |
-    | Configuration for PostgreSQL database. Make sure to install the driver
-    | from npm when using this connection
-    |
-    | npm i pg
-    |
-    */
     pg: {
       client: 'pg',
-      connection:{
-      host: Env.get('DB_HOST', DATABASE_URL.host),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', DATABASE_URL.username),
-      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))},
-      healthCheck: false,
-      debug: false,
+      connection: Application.inProduction ?
+      Env.get('DATABASE_URL') + "?ssl=no-verify":
+      {
+        host: Env.get('PG_HOST'),
+        port: Env.get('PG_PORT'),
+        user: Env.get('PG_USER'),
+        password: Env.get('PG_PASSWORD', ''),
+        database: Env.get('PG_DB_NAME'),
+      },
+    
+      healthCheck: Application.inDev,
+			debug: Application.inDev,
     },
+
+/*
+I usually use this custom connection to run database migrations on remote database from my local machine. For instance,
+node ace migration:run --connection=custom, this will run the migration against you remote database. In order to use this connection, you must set DATABASE_URL in .env file, you can get the value from your heroku dashboard, or by running this command on your terminal: heroku config:get DATABASE_URL --app=your_app_name
+*/
+    custom: {
+      client: "pg",
+      connection: Env.get("DATABASE_URL") + "?ssl=no-verify",
+    },
+
   },
 
-  /*
-  |--------------------------------------------------------------------------
-  | ORM Configuration
-  |--------------------------------------------------------------------------
-  |
-  | Following are some of the configuration options to tweak the conventional
-  | settings of the ORM. For example:
-  |
-  | - Define a custom function to compute the default table name for a given model.
-  | - Or define a custom function to compute the primary key for a given model.
-  |
-  */
-  orm: {},
+  orm: {
+  },
 }
 
 export default databaseConfig
